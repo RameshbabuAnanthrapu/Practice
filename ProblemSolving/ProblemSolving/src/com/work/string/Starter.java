af7e08c6b83f5e3c90106a8c5fb521830ac1a5d7
Sindhu <Sindhu@192.168.100.12>
2020-02-24 11:54:12 +0530

    string with spaces

--- ProblemSolving/ProblemSolving/src/com/work/string/Starter.java
+++ ProblemSolving/ProblemSolving/src/com/work/string/Starter.java
@@ -5,11 +5,15 @@ public class Starter {
 	
 	public  static void main(String[] args) {
 		
-		String s1 = "abbcdabb";
+		//String s1 = "abbcdabb";
+		String s2 = "MY NAME IS RAMESH";
+		
+		
 		
 		
 		TestSubString subStrinTester = new TestSubString();
-		subStrinTester.printAllSubStrings(s1);
+		//subStrinTester.printAllSubStrings(s1);
+		subStrinTester.testStringWithSpaces(s2);
 		
 	}
 
--- ProblemSolving/ProblemSolving/src/com/work/string/TestSubString.java
+++ ProblemSolving/ProblemSolving/src/com/work/string/TestSubString.java
@@ -12,6 +12,11 @@ public class TestSubString {
 	
 	*/
 	
+	String tmp ;
+	int distCount = 0;
+	int longestSubStringLength = 0;
+	String longestSubString;
+	
 	
 	public void printAllSubStrings(String s1){
 		
@@ -21,12 +26,86 @@ public class TestSubString {
 			 for(int j=i;j<s1.length();j++) {
 				 System.out.println("start and end  indexes " +i +","+j);
 				 System.out.println( s1.substring(i, j));
+				  printLongestSubString(s1.substring(i, j));
 				 subStringCount++;
 			 }
 			
 		}
 		
 		System.out.println("Number of substrings are::" +subStringCount);
+		System.out.println("longest substring with two distint chars" +longestSubString);
+		
+	}
+	
+	private void  printLongestSubString(String s2) {
+		
+		char[] charArray = s2.toCharArray();
+		
+		
+		
+		for (int i=0;i<s2.length();i++) {
+			
+			for(int j=i+1; j< s2.length(); j++) {
+				tmp = String.valueOf(s2.charAt(j-1));
+				if(!String.valueOf(charArray[i]).equals(String.valueOf(charArray[j]))
+						&& tmp.equals(String.valueOf(charArray
+								[j]))) 
+				{
+					tmp = String.valueOf(charArray[j]);
+					distCount++;	
+				}
+				
+			}
+			
+		}
+		
+		if(distCount ==2  && longestSubStringLength< s2.length()) {
+			
+			longestSubStringLength = s2.length();
+			longestSubString = s2;
+		}
+		
+		
+		
+		
+		 
+		
+		
+		
+		
+	}
+	
+	public void testStringWithSpaces(String st){
+		
+		char[] input =  st.toCharArray();
+		char[] output = new char[input.length];
+		
+		for(int i=0; i<input.length;i++) {
+			if(input[i] == ' ') {
+				output[i] = ' ';
+			}
+		}
+		
+		int j = input.length-1;
+		for(int i =0; i<input.length;i++) {
+			
+			if(input[i]!=' ') {
+				
+				if(output[j]==' ') {
+					j--;	
+				}
+				output[j]=input[i];
+				j--;
+				
+				
+			}
+			
+		}
+		
+		//String str;
+		System.out.println(String.copyValueOf(output));
+		
+		
 		
 	}
 
